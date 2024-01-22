7d68546863cb7e56ce93c05e35bca81fd9fd1d0c
DevHouda <devhouda@gmail.com>
2024-01-22 08:28:22 +0100

    backend done

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+//mongod, mongo
+
+export const mongoDBURL = "mongodb://localhost:27017/book-store-db";
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// npm run dev to run our server
+// we added type: module to package.json to enable es6
+// and start and dev in scripts to use nodemon
